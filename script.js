1e14f69f5049720bfe5a315058564a737397a44e
HeitorDalla <heitorvillavilla@gmail.com>
2025-05-14 20:04:18 -0300

    Finalizado todos os modos de fazer requisicoes de API

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 
 //     requisicao.onload = () => {
 //         if (requisicao.status === 200) {
-//             let respostaAPI = JSON.parse(requisicao.response);
+//             let data = JSON.parse(requisicao.response);
 
 //             if (respostaAPI.erro) {
 //                 alert("CEP não encontrado!");
@@ -25,10 +25,10 @@
 //                 return;
 //             }
 
-//             document.querySelector("#logradouro").value = respostaAPI.logradouro;
-//             document.querySelector("#municipio").value = respostaAPI.localidade;
-//             document.querySelector("#bairro").value = respostaAPI.bairro;
-//             document.querySelector("#uf").value = respostaAPI.uf;
+//             document.querySelector("#logradouro").value = data.logradouro;
+//             document.querySelector("#municipio").value = data.localidade;
+//             document.querySelector("#bairro").value = data.bairro;
+//             document.querySelector("#uf").value = data.uf;
 //         } else {
 //             alert("Erro ao consultar CEP!");
 //         }
@@ -51,15 +51,46 @@
 //     const url = `https://viacep.com.br/ws/${cep}/json/`;
 
 //     fetch(url)
-//     .then((response) => {
-//         return response.json();
-//     }).then((data) => {
-//         console.log(data)
-//         document.querySelector("#logradouro").value = data.logradouro;
-//         document.querySelector("#municipio").value = data.localidade;
-//         document.querySelector("#bairro").value = data.bairro;
-//         document.querySelector("#uf").value = data.uf;
-//     })
+//         .then((response) => {
+//             return response.json();
+//         }).then((data) => {
+//             document.querySelector("#logradouro").value = data.logradouro;
+//             document.querySelector("#municipio").value = data.localidade;
+//             document.querySelector("#bairro").value = data.bairro;
+//             document.querySelector("#uf").value = data.uf;
+//         })
 // }
+
+// Utilizando a biblioteca Axios
+function buscarCEP () {
+    const value = document.querySelector("#inserir").value;
+    const cep = value.replace(/\D/g, '');
+
+    if (!(cep.length === 8)) {
+        alert("Por favor, insira um CEP válido!");
+        document.querySelector("#inserir").value = '';
+        return;
+    }
+
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    axios.get(url)
+        .then((response) => {
+            if (response.status === 200) {
+                const data = response.data;
+
+                document.querySelector("#logradouro").value = data.logradouro;
+                document.querySelector("#municipio").value = data.localidade;
+                document.querySelector("#bairro").value = data.bairro;
+                document.querySelector("#uf").value = data.uf;
+            } else {
+                alert("Erro no servidor!");
+                return;
+            }
+        })
+        .catch(() => {
+            alert("Erro ao consultar o CEP!");
+        })
+}
 
 document.querySelector("#consultar").addEventListener("click", buscarCEP);
